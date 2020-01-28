293904dc4fabcf25aa42e2693653c851bab5bd51
Nzubechukwu <nzubennamani@gmail.com>
2020-01-28 07:58:00 +0100

    added procfile

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "regenerator-runtime/runtime";
 import express from 'express';
 
 
@@ -16,10 +17,7 @@ app.get('/', (req, res) => {
 })
 app.use(router);
 
-app.listen(port, err => {
-    if(err) {
-        console.error(err);
-    } else {
-        console.log(`Server is running port ${port}`)
-    }
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
 })
+
